185a01708bd047092d3850cdf13d47c4a577ba98
Евгений Есауленко <esaulenko.lenovo@gmail.com>
2016-03-14 00:44:02 +0300

    project base created

---
+++ JsonGenerator/JsonGenerator/GeneratorBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace JsonGenerator
+{
+    class GeneratorBase
+    {
+        public void GenerateJson()
+        {
+            if (!Directory.Exists(@"C:\JsonGenerator"))
+                Directory.CreateDirectory(@"C:\JsonGenerator");
+
+            if (Directory.Exists(@"C:\JsonGenerator"))
+            {
+                try
+                {
+                    File.Delete(@"C:\JsonGenerator\students.json");
+                }
+                catch { }
+            }
+
+            Random random = new Random((int)DateTime.Now.Ticks);
+
+            Students student = new Students(Randomizer.GetRandomeName(random), Randomizer.GetRandomSubject(random), Randomizer.GetRandomMark(random));
+            Students[] students = new Students[] { student };
+
+            int randomNumber = random.Next(10, 25);
+            for (int i = 1; i <= randomNumber; i++)
+            {
+                Array.Resize(ref students, students.Length + 1);
+                students[students.Length - 1] = new Students(Randomizer.GetRandomeName(random), Randomizer.GetRandomSubject(random), Randomizer.GetRandomMark(random));
+            }
+
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Students[]));
+            using (FileStream stream = new FileStream(@"C:\JsonGenerator\students.json", FileMode.OpenOrCreate))
+            {
+                jsonFormatter.WriteObject(stream, students);
+            }
+        }
+    }
+
+    [DataContract]
+    public class Students
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Subject { get; set; }
+        [DataMember]
+        public int Mark { get; set; }
+
+        public Students(string name, string subject, int mark)
+        {
+            Name = name;
+            Subject = subject;
+            Mark = mark;
+        }
+    }
+}
---
+++ JsonGenerator/JsonGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Press any key to generate JSON:");
+            while (true)
+            {
+                Console.ReadKey();
+                GeneratorBase gb = new GeneratorBase();
+                gb.GenerateJson();
+                Console.WriteLine(@"C:\JsonGenerator\students.json was generated");
+                Console.Write("\nPress any key to generate JSON:");
+            }
+        }
+    }
+}
